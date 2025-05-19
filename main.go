33f99571cc1a25605d9d4221b9ad46d3b7643a6b
Asknuo <3263296956@qq.com>
2025-05-19 19:06:47 +0800

    表建构搭建与flag命令

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"sever/core"
+	"sever/flag"
 	"sever/global"
 	"sever/initialize"
 )
@@ -11,11 +12,11 @@ func main() {
 	global.Log = core.InitLogger()
 	initialize.OtherInit()
 	global.DB = initialize.InitGorm()
-	global.Redis = initialize.ConnectRedis()
+	//global.Redis = initialize.ConnectRedis()
 	global.ESClient = initialize.ConnectEs()
 
 	defer global.Redis.Close()
-
+	flag.InitFlag()
 	initialize.InitCron()
 
 	core.RunServer()
